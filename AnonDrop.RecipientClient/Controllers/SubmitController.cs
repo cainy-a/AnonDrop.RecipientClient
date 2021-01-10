ae3a672736c82e23341dad9aa32e76538e8e50c0
cainy-a <cainy-a@users.noreply.github.com>
2021-01-10 21:45:10 +0000

    Client

---
+++ AnonDrop.RecipientClient/Controllers/SubmitController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnonDrop.RecipientClient.Controllers
+{
+	[Route("/")]
+	public class SubmitController : ControllerBase
+	{
+		private readonly Random _rand = new Random();
+
+		[HttpPost("submit")]
+		public IActionResult Submit(string link, string password)
+		{
+			if (string.IsNullOrWhiteSpace(link)
+			 || string.IsNullOrWhiteSpace(password))
+				return ValidationProblem();
+
+			var filePath = Path.Combine(Environment.CurrentDirectory, "anondrop_data.json");
+
+			var data = new string[][] { };
+
+			if (System.IO.File.Exists(filePath))
+			{
+				var sr = new StreamReader(filePath);
+				data = JsonSerializer.Deserialize<string[][]>(sr.ReadToEnd());
+			}
+			else
+			{
+				System.IO.File.CreateText(filePath);
+			}
+
+			data = data.Append(new[] {link, password}).ToArray();
+
+			var shuffledEnumerable = data.Shuffle(_rand);
+			var shuffledArray =
+				(from item in shuffledEnumerable
+				 select item.ToArray()).ToArray();
+			var serialised = JsonSerializer.Serialize(shuffledArray);
+
+			System.IO.File.WriteAllText(filePath, serialised);
+
+			return Ok();
+		}
+	}
+}
---
+++ AnonDrop.RecipientClient/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnonDrop.RecipientClient
+{
+	public static class ExtensionMethods
+	{
+		public static IEnumerable<IEnumerable<string>> Shuffle(this IEnumerable<IEnumerable<string>> enumerable,
+		                                                       Random                                rand)
+		{
+			var items = enumerable as IEnumerable<string>[] ?? enumerable.ToArray();
+
+			var working = new IEnumerable<string>[items.Length];
+			foreach (var item in items)
+			{
+				var randomInt = -1;
+				while (!(working.ElementAtOrDefault(randomInt) ?? Array.Empty<string>()).Any())
+					randomInt = rand.Next(items.Length);
+
+				working[randomInt] = item;
+			}
+
+			return working;
+		}
+	}
+}
